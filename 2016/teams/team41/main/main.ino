903d41cdb50a34c1dc1ad91e2b11da48dca8f130
dkobets <dkobets@06e43772-ae1b-4c0d-9892-c0dabbf93334>
2016-11-20 00:47:30 +0000

    Added Wire_Util.ino file from Rollen's repository Added accelerometer-controlled snake movement -- based on the tilt position of the board, the snake moves left/right/up/down  - current issues: whenever the board is held at a tipping point between directions (i.e., right-up) a slight movement can cause it to rapidly switch between those two  directions removed - lower-left button, as it is no longer necessary. It could be re-added later if necessary

--- 2016/teams/team41/main/main.ino
+++ 2016/teams/team41/main/main.ino
@@ -14,33 +14,30 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <Wire.h>
 /*
 TODO:
 fix global variables vs local variables created in each loop() call... for example: lastUpdatedInput.
 coords are being copied around instead of being kept as pointers
 change game structure to revolve around a 2D array instead of a linked list
-refactor button code
-merge button "handler" with the current button structure
-comment "moveSnake()" function
 */
 
 void onButtonDown(void);
-void setupLeftButton(void);
 void initializeOLED(void);
 
 struct Input *updateInputs(void); 
 
-int genRandNum(int n);
-
+void appendToHead(struct point*);
+void appendToHead(int,int);
 void initializeSnake(struct point[], int);
+
 void drawSnake(void);
 void eraseTail(void);
 void drawHead(void);
 
-void appendToHead(int, int); 
-
-void drawPixel(struct point*); 
-void drawPixel(int, int); 
+int genRandNum(int n);
+boolean intersectsWithSnake(struct Segment *head, struct point* point);
+boolean pointsIntersect(struct point* point1, struct point* point2);
 
 /*
  * Input
@@ -68,21 +65,36 @@ struct point {
   int y;
 } food;
 
-struct snakePart {
-	struct point coords;
-	struct snakePart* prev;
+/*
+ * Snake components
+ */
+struct Segment {
+  struct point coords;
+  struct Segment *next; // towards head
+  struct Segment *prev; // towards tail
 };
+const int NUM_POINTS =9;
+struct point points[NUM_POINTS] = {
+    {11, 11}, 
+    {12, 11}, 
+    {13, 11}, 
+    {14, 11}, 
+    {15, 11}, 
+    {16, 11}, 
+    {17, 11}, 
+    {18, 11}, 
+    {19, 11}};
+
+struct Segment *head = NULL;
+struct Segment *tail = NULL;
+struct Segment *current = NULL;
+
+boolean isGrowing = false;
 
 const int X_BOUND_LEFT=0;
 const int X_BOUND_RIGHT=127;
 const int Y_BOUND_TOP=0;
 const int Y_BOUND_BOTTOM=31;
-
-char map[Y_BOUND_BOTTOM+1][X_BOUND_RIGHT+1] = {0};
-
-struct snakePart* head = NULL;
-struct snakePart* tail = NULL;
-boolean isGrowing = false;
 
 enum direction {
   UP,
@@ -91,39 +103,28 @@ enum direction {
   RIGHT
 } direction=RIGHT;
 
-void setup() {
 
-  Serial.begin(9600);
+float LSBperG = 128.0;
+
+void setup() {
+  WireInit();
+  delay(200);
+  
+  WireWriteRegister(0x1D, 0x2D, 1 << 3); // set standby in the POWER_CTL register
+  WireWriteRegister(0x1D, 0x31, 1); // set 10-bit res with 4g range
 
   initializeOLED();
-
-  for (int i = 0; i < NUM_INPUTS; i ++) {
-    pinMode(INPUTS[i].code, INPUT);
-  }
   
-	initGame();
+  initializeSnake(points, NUM_POINTS); 
+  generateFood();
+  drawSnake();
+  drawFood();
   OrbitOledUpdate();
 }
 
 void loop() {
-  
-  lastUpdatedInput = updateInputs();
-  if (lastUpdatedInput) {
-    switch(lastUpdatedInput->code) {
-      case PD_2: //BTN_1
-        if (lastUpdatedInput->active && direction != UP)
-          direction = DOWN;
-        break;
-      case PE_0: //BTN_2
-        if (lastUpdatedInput->active && direction != DOWN)
-          direction = UP;
-        break;
-      case PF_0: //SW_2
-        if (lastUpdatedInput->active && direction != LEFT)
-          direction = RIGHT;
-        break;
-    }
-  } 
+
+  updateDirection();
   moveSnake();
   
   OrbitOledUpdate();
@@ -131,36 +132,122 @@ void loop() {
   DelayMs(1000/FPS);
 }
 
+void updateDirection() {
+  uint32_t data[6] = {0};
+  WireWriteByte(0x1D, 0x32);
+  WireRequestArray(0x1D, data, 6);
+
+  uint16_t xi = (data[1] << 8) | data[0];
+  uint16_t yi = (data[3] << 8) | data[2];
+  uint16_t zi = (data[5] << 8) | data[4];
+
+  
+  float x = *(int16_t*)(&xi) / LSBperG;
+  float y = *(int16_t*)(&yi) / LSBperG;
+  float z = *(int16_t*)(&zi) / LSBperG;
+
+  float pitch = (atan2(x,sqrt(y*y+z*z)) * 180.0) / PI;
+  float roll = (atan2(y,sqrt(x*x+z*z)) * 180.0) / PI;
+
+  if (fabs(pitch) <= 10 && fabs(roll) <= 10) {
+    return;
+  } else if (pitch > 0 && roll > 0) { // lower left
+    if (pitch >= roll && direction != RIGHT) {
+      direction = LEFT;
+    } else if (direction != UP) {
+      direction = DOWN;
+    }
+  } else if (pitch > 0 && roll < 0) {
+    if (pitch >= fabs(roll) && direction != RIGHT) {
+      direction = LEFT; 
+    } else if (direction != DOWN) {
+      direction = UP;
+    }
+  } else if (pitch < 0 && roll < 0) {
+    if (pitch <= roll && direction != LEFT) {
+      direction = RIGHT;
+    } else if (direction != DOWN) {
+      direction = UP;
+    }
+  } else {
+    if (fabs(pitch) >= roll && direction != LEFT) {
+      direction = RIGHT;
+    } else if (direction != UP) {
+      direction = DOWN;
+    }
+  }
+}
+
+void appendToHead(int x, int y) {
+  struct point newPoint = {x, y};
+  appendToHead(&newPoint);
+}
+
+void appendToHead(struct point *coords) {
+  struct Segment* newHead = (struct Segment*)malloc(sizeof(struct Segment));
+  newHead->coords = *coords;
+  
+  newHead->next = NULL;
+  newHead->prev = head;
+  
+  if (head) {
+    head->next = newHead;
+    if (!tail) {
+      tail = head;
+    }
+  }
+  head = newHead;
+}
+
+/*
+ * Create a linked list based off a list of coordinates
+ */
+void initializeSnake(struct point coords[], int numCoords) {
+  for (int i = 0; i < numCoords; i ++) {
+    appendToHead(&coords[i]);
+  }
+}
+
+/*
+*/
 void moveSnake() {
-	switch(direction) {
-		case LEFT:
-			head.x --;
-			break;
-		case RIGHT:
-			head.x ++;
-			break;
-		case UP:
-			head.y --;
-			break;
-		case DOWN:
-			head.y ++;
-			break;
-	}
+  if (isGrowing) {
+    switch(direction) {
+      case LEFT:
+        appendToHead(head->coords.x-1, head->coords.y);
+        break;
+      case RIGHT:
+        appendToHead(head->coords.x+1, head->coords.y);
+        break;
+      case UP:
+        appendToHead(head->coords.x, head->coords.y-1);
+        break;
+      case DOWN:
+        appendToHead(head->coords.x, head->coords.y+1);
+        break;
+    }
+    isGrowing = false;
+  } 
   // To move the snake, the tail node has its coordinates moved ahead of the head (in some direction) and becomes the new head. The node that was previously ahead of the tail
   // becomes the new tail.
-	if (!isGrowing)
-		eraseTail();
   else {
     eraseTail();
     switch(direction) {
       case LEFT:
-				head
+        tail->coords.x = head->coords.x-1;
+        tail->coords.y = head->coords.y;
         break;
       case RIGHT:
+        tail->coords.x = head->coords.x+1;
+        tail->coords.y = head->coords.y;
         break;
       case UP:
+        tail->coords.y = head->coords.y-1;
+        tail->coords.x = head->coords.x;
         break;
       case DOWN:
+        tail->coords.y = head->coords.y+1;
+        tail->coords.x = head->coords.x;
         break;
     }
     // swap the head node with the tail node
@@ -173,13 +260,13 @@ void moveSnake() {
     head = current;
   }
 
-  if (head->coords.x >= X_BOUND_RIGHT) {
+  if (head->coords.x > X_BOUND_RIGHT) {
     head->coords.x = 0;
   }
   else if (head->coords.x < X_BOUND_LEFT) {
     head->coords.x = X_BOUND_RIGHT-1;
   }
-  if (head->coords.y >= Y_BOUND_BOTTOM) {
+  if (head->coords.y > Y_BOUND_BOTTOM) {
     head->coords.y = 0;
   }
   if (head->coords.y < Y_BOUND_TOP) {
@@ -239,7 +326,6 @@ struct Input *updateInputs(void) {
   active = 0;
   for (int i = 0; i < NUM_INPUTS; i ++) {
     active = digitalRead(INPUTS[i].code);
-
     if (active != INPUTS[i].active) {
       lastUpdatedInput = &INPUTS[i];
       INPUTS[i].active = active;
@@ -249,10 +335,27 @@ struct Input *updateInputs(void) {
 }
 
 /*
-* Generates a random number between 0 and 'n' inclusive
+* Generates a random number between 0 and @n inclusive
 */
 int genRandNum(int n){
   return rand()%(n+1);
+}
+
+boolean intersectsWithSnake(struct Segment *head, struct point* point) {
+  while (head) {
+    if (pointsIntersect(point, &(head->coords))) {
+      return true;
+    }
+    head = head->prev;
+  }
+  return false;
+}
+
+/*
+* Return true if two points have identical x and y components; Otherwise, return false
+*/
+boolean pointsIntersect(struct point* point1, struct point* point2) {
+  return point1->x == point2->x && point1->y == point2->y;
 }
 
 /*
@@ -260,31 +363,28 @@ int genRandNum(int n){
 */
 void generateFood(void) {
   // make sure the food does not spawn within the snake or within one pixel of its head or tail
+  current = head;
   do {
     food.x = genRandNum(X_BOUND_RIGHT);
     food.y = genRandNum(Y_BOUND_BOTTOM);
   } while (!validateFoodLocation(&food));
-	map[food.y][food.x] = 1;
 
   drawFood();
 }
 /*
-* Checks if the attempted food spawn location intersects with the snake or within one pixel of any of its segments
+* Checks if the attempted food spawn location intersects with the snake or within one pixel of each of its segments
 * return true if no intersections were found; Otherwise, return false
 */
 boolean validateFoodLocation(struct point* food) {
-	if (map[food->y][food->x] == 1 || 
-		map[food->y-1][food->x-1] == 1 || 
-		map[food->y][food->x-1] == 1 ||
-		map[food->y+1][food->x-1] == 1 ||
-		map[food->y+1][food->x] == 1 ||
-		map[food->y+1][food->x+1] == 1 ||
-		map[food->y][food->x+1] == 1 ||
-		map[food->y-1][food->x+1] == 1 ||
-		map[food->y-1][food->x] == 1 ||
-	) 
-		return false;
-	return true;
+  current = head;
+  while (current) {
+    if (pointsIntersect(&(current->coords), food))
+      return false;
+    if (abs(current->coords.x - food->x) == abs(current->coords.y - food->y) && abs(current->coords.x - food->x) == 1)
+      return false;
+    current = current->prev;
+  }
+  return true;
 }
 
 void eraseFood(void){
@@ -298,88 +398,3 @@ void drawFood(){
   OrbitOledDrawPixel();
 }
 
-/*
-* Initialize game elements such as the map and the food starting location
-*/
-void initGame(void) {
-	isGrowing = false;
-	memset(map, 0, (X_BOUND_RIGHT+1)*(Y_BOUND_BOTTOM+1));
-
-	appendToHead(11, 17);
-	appendToHead(12, 17);
-	appendToHead(13, 17);
-	appendToHead(14, 17);
-	appendToHead(15, 17);
-	appendToHead(16, 17);
-	appendToHead(17, 17);
-
-	struct snakePart current* = head;
-	while (current) {
-		drawPixel(current);
-		current = current->prev;
-	}
-
-	generateFood();
-	direction = RIGHT;
-}
-
-void appendToHead(int x, int y) {
-  struct snakePart* newHead = (struct Segment*)malloc(sizeof(struct Segment));
-  newHead->coords = {.x=x,.y=y};
-
-	newHead->prev = head;
-
-  if (head) {
-    if (!tail) {
-      tail = head;
-    }
-  }
-  head = newHead;
-}
-
-void drawPixel(struct point* point) {
-	drawPixel(point->x, point->y);
-}
-void drawPixel(int x, int y) {
-	OrbitOledSetDrawMode(modOledSet);
-	OrbitOledMoveTo(x, y);
-	OrbitOledDrawPixel();
-}
-
-/*
- * Create a linked list based off a list of coordinates
- */
-void initializeSnake(struct point coords[], int numCoords) {
-  for (int i = 0; i < numCoords; i ++) {
-    appendToHead(&coords[i]);
-  }
-}
-
-void setupLeftButton(void) {
-  // SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);        // Enable port F
-  GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);  // Init PF4 as input
-  GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4,
-      GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);  // Enable weak pullup resistor for PF4
-
-  // Interrupt setup
-  GPIOIntDisable(GPIO_PORTF_BASE, GPIO_PIN_4);        // Disable interrupt for PF4 (in case it was enabled)
-  GPIOIntClear(GPIO_PORTF_BASE, GPIO_PIN_4);      // Clear pending interrupts for PF4
-  GPIOIntRegister(GPIO_PORTF_BASE, onButtonDown);     // Register our handler function for port F
-  GPIOIntTypeSet(GPIO_PORTF_BASE, GPIO_PIN_4,
-      GPIO_FALLING_EDGE);             // Configure PF4 for falling edge trigger
-  GPIOIntEnable(GPIO_PORTF_BASE, GPIO_PIN_4);     // Enable interrupt for PF4
-  direction = RIGHT;
-}
-void onButtonDown(void) {
-    if (GPIOIntStatus(GPIO_PORTF_BASE, false) & GPIO_PIN_4) {
-        // PF4 was interrupt cause
-        if (direction!=RIGHT){
-					direction = LEFT;
-        }
-        // active = 1;
-        //GPIOIntRegister(GPIO_PORTF_BASE, onButtonUp);   // Register our handler function for port F
-        GPIOIntTypeSet(GPIO_PORTF_BASE, GPIO_PIN_4,
-            GPIO_RISING_EDGE);          // Configure PF4 for rising edge trigger
-        GPIOIntClear(GPIO_PORTF_BASE, GPIO_PIN_4);  // Clear interrupt flag
-    }
-}
